7856cf741e143561cfa54e332b0331292d31ccae
italomoraess <italomoraes.sth@gmail.com>
2023-07-05 19:00:02 -0300

    finished tela login

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { 
   Text,
   View,
@@ -9,6 +10,18 @@ import {
 import { styles } from './styles';
 
 export default function App() {
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  const handleLoginBtn = () => {
+    console.log(emailField)
+    console.log(passwordField) 
+  }
+
+  const handleForgetBtn = () => {}
+  
+  const handleSignUpBtn = () => {}
+
   return (
     <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
@@ -18,41 +31,50 @@ export default function App() {
         <Text style={styles.h2}>Bem vindo! Digite seus dados abaixo.</Text>
 
         <View style={styles.inputArea}>
-          <Text style={styles.inputLabel}>Email:</Text>
+          <Text style={styles.inputLabel}>Email</Text>
           <TextInput 
             style={styles.inputField}
             placeholder='Digite seu email'
+            placeholderTextColor='#999'
+            value={emailField}
+            onChangeText={(text) => setEmailField(text)}
+            autoCapitalize='none'
+            keyboardType='email-address'
           />
         </View>
 
         <View style={styles.inputArea}>
-          <Text style={styles.inputLabel}>Senha:</Text>
+          <Text style={styles.inputLabel}>Senha</Text>
           <TextInput 
             style={styles.inputField}
             placeholder='*******'  
+            placeholderTextColor='#999'
+            secureTextEntry
+            value={passwordField}
+            onChangeText={(text) => setPasswordField(text)}
           />
         </View>
 
         <View style={styles.aditionals}>
-          <TouchableOpacity style={styles.forgotBtnArea}>
+          <TouchableOpacity style={styles.forgotBtnArea} onPress={handleForgetBtn}>
             <Text style={styles.forgotBtnText}>Esqueci minha senha</Text>
           </TouchableOpacity>
         </View>  
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleLoginBtn}>
           <Text style={styles.buttonText}>Entrar</Text>
         </TouchableOpacity>
 
         <View style={styles.signUpArea}>
           <Text style={styles.signUpText}>Não tenho conta?</Text>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleSignUpBtn}>
             <Text style={styles.signUpBtnText}>Cadastre-se</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.footerArea}>
           <Text style={styles.footerText}>
-            Criado por Italo
+            Criado por Italo Moraes
           </Text>
         </View>
         
